df48a2a2ac32345a792d4db490030871f39abd25
alamin <alaminhosen44000@gmail.com>
2023-09-03 16:04:54 +0600

    c

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
     $(".offcanvas-menu").addClass("active");
     $(".offcanvas-overlay").addClass("active");
   });
-  $(".offcanvas-menu a").click(function () {
+  $(".offcanvas-menu a").click(function (e) {
+    // e.preventDefault();
     $(".offcanvas-menu").removeClass("active");
     $(".offcanvas-overlay").removeClass("active");
   });
@@ -25,35 +26,35 @@
   });
 
   // fullPage
-  $("[data-aos]").each(function () {
-    $(this).addClass("aos-init");
-  });
-  new fullpage("#fullpage", {
-    // sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
-    anchors: [
-      "page1",
-      "page2",
-      "page3",
-      "page4",
-      "page5",
-      "page6",
-      "page7",
-      "page8",
-    ],
-    // navigationTooltips: ["fullPage", "Open", "Easy", "Touch"],
-    css3: true,
-    scrollingSpeed: 1000,
-    navigation: false,
-    slidesNavigation: true,
-    responsiveHeight: 330,
-    responsiveWidth: 992,
-    dragAndMove: true,
-    dragAndMoveKey:
-      "YkdZV3gyWVhKdmRISnBaMjh1WTI5dEE3X09keFpISmhaMEZ1WkUxdmRtVT0xbmw=",
-    controlArrows: false,
-    // Get your license at https://alvarotrigo.com/fullPage/pricing/
-    // licenseKey: "YOUR LICENSE KEY HERE ",
-  });
+  // $("[data-aos]").each(function () {
+  //   $(this).addClass("aos-init");
+  // });
+  // new fullpage("#fullpage", {
+  //   // sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+  //   anchors: [
+  //     "page1",
+  //     "page2",
+  //     "page3",
+  //     "page4",
+  //     "page5",
+  //     "page6",
+  //     "page7",
+  //     "page8",
+  //   ],
+  //   // navigationTooltips: ["fullPage", "Open", "Easy", "Touch"],
+  //   css3: true,
+  //   scrollingSpeed: 1000,
+  //   navigation: false,
+  //   slidesNavigation: true,
+  //   responsiveHeight: 330,
+  //   responsiveWidth: 992,
+  //   dragAndMove: true,
+  //   dragAndMoveKey:
+  //     "YkdZV3gyWVhKdmRISnBaMjh1WTI5dEE3X09keFpISmhaMEZ1WkUxdmRtVT0xbmw=",
+  //   controlArrows: false,
+  //   // Get your license at https://alvarotrigo.com/fullPage/pricing/
+  //   // licenseKey: "YOUR LICENSE KEY HERE ",
+  // });
 })(jQuery);
 
 $(document).ready(function () {
